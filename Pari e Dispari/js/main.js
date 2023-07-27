6094443032727d3c6181aea4ed25aa43f66adceb
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-27 17:22:46 +0200

    feat: Pari e Dispari

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,2 +1,39 @@
+const giocataUser = prompt('Scegli "pari" o "dispari"').toLowerCase();
+const numUser = parseInt(prompt('Inserisci un numero intero tra 1 e 5'));
 
 
+function numRandom () {
+    const numeroRandom = Math.floor(Math.random() * 5) + 1;
+    
+    return numeroRandom;
+}
+
+const random = numRandom ();
+
+console.log('Hai scommesso sul: ' + giocataUser);
+console.log('Il numero che hai scelto è: ' + numUser);
+
+console.log('Il numero del pc è: ' + random);
+
+const sum = numUser + random;
+console.log('La somma dei due numeri è: ' + sum);
+
+function pariDispari () {
+
+    let parDis;
+    if (sum % 2 == 0) {
+        parDis = ('pari');       
+    } else {
+        parDis = ('dispari');        
+    }
+    return parDis;
+}
+
+const risultato = pariDispari ();
+console.log('Il numero è: ' + risultato);
+
+if (giocataUser == risultato) {
+    console.log('HAI VINTO');
+} else {
+    console.log('HAI PERSO');
+}
